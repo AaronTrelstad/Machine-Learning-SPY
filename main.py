04355fc33d922da765bcc8e2bf36b472d591b42c
Aaron Trelstad <108637601+AaronTrelstad@users.noreply.github.com>
2023-04-14 17:33:01 +0000

    edit #4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from arrays import *
-from middles import *
-from deviations import *
+from functions.arrays import *
+from functions.middles import *
+from functions.deviations import *
 
 if __name__ == "__main__":
     request = int(input("1. 1 Year    2. 30 days    3. 60 days "))
