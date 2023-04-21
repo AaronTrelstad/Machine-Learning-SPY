aa9b8c349b9ba3aa9e4890f6865879f5b912516a
Aaron Trelstad <108637601+AaronTrelstad@users.noreply.github.com>
2023-04-21 23:52:09 +0000

    edit

--- functions/polyReg.py
+++ functions/polyReg.py
@@ -2,15 +2,15 @@ from arrays import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-days = 30
+days = 200
 
 x = []
 for idx, num in enumerate(dates):
   x.append(idx + 1)
 
 n = 3
-mymodel = np.poly1d(np.polyfit(x[-days:], percentChange[-days:], n))
+mymodel = np.poly1d(np.polyfit(x[-days:], dayClose[-days:], n))
 
-plt.scatter(x[-days:], percentChange[-days:])
+plt.scatter(x[-days:], dayClose[-days:])
 plt.plot(x[-days:], mymodel(x[-days:]))
-plt.savefig("poly_reg30PercentChange.png")
+plt.savefig("poly_regDayClose.png")
